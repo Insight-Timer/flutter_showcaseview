2061fcb91374ea2d4f973d79b80db3c289115a12
Ravi <dhorajiya104@gmail.com>
2021-03-18 10:10:00 +0530

    FIX: Add tooltip arrow

--- lib/showcase.dart
+++ lib/showcase.dart
@@ -317,3 +317,53 @@ class _TargetWidget extends StatelessWidget {
 }
 
 enum ArrowType { up, down }
+
+class TooltipShapeBorder extends ShapeBorder {
+  final double arrowWidth;
+  final double arrowHeight;
+  final double arrowArc;
+  final double radius;
+  final ArrowType type;
+
+  TooltipShapeBorder({
+    this.radius = 20.0,
+    this.arrowWidth = 20.0,
+    this.arrowHeight = 10.0,
+    this.arrowArc = 0.5,
+    this.type = ArrowType.up,
+  }) : assert(arrowArc <= 1.0 && arrowArc >= 0.0);
+
+  @override
+  EdgeInsetsGeometry get dimensions => EdgeInsets.only(top: arrowHeight);
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection textDirection}) => null;
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection textDirection}) {
+    rect = Rect.fromPoints(rect.topLeft, rect.bottomRight - Offset(0, arrowHeight));
+    // ignore: omit_local_variable_types
+    double x = arrowWidth, y = arrowHeight, r = 1 - arrowArc;
+    if (type == ArrowType.up) {
+      return Path()
+        ..addRRect(RRect.fromRectAndRadius(rect, Radius.circular(radius)))
+        ..moveTo(rect.topLeft.dx + 56, rect.topCenter.dy)
+        ..relativeLineTo(-x / 2 * r, -y * r)
+        ..relativeQuadraticBezierTo(-x / 2 * (1 - r), y * (-1 + r), -x * (1 - r), 0)
+        ..relativeLineTo(-x / 2 * r, y * r);
+    } else {
+      return Path()
+        ..addRRect(RRect.fromRectAndRadius(rect, Radius.circular(radius)))
+        ..moveTo(rect.bottomLeft.dx + 38, rect.bottomCenter.dy)
+        ..relativeLineTo(-x / 2 * r, y * r)
+        ..relativeQuadraticBezierTo(-x / 2 * (1 - r), y * (1 - r), -x * (1 - r), 0)
+        ..relativeLineTo(-x / 2 * r, -y * r);
+    }
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {}
+
+  @override
+  ShapeBorder scale(double t) => this;
+}
